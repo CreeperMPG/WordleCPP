aebec7c256bb600c1f17899aa47fe80eada566ca
CreeperMPG <148458119+CreeperMPG@users.noreply.github.com>
2024-01-22 23:01:34 +0800

    Update main.cpp to Version 1.1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
-#include <windows.h>
 #include <conio.h>
+#include <windows.h>
 using namespace std;
 
 const int Word_Length = 5;
-const string Version = "1.0";
+const string Version = "1.1";
 const vector<string> words = {
     "aback", "abate", "abbot", "abhor", "abide", "abode", "abort", "about",
     "above", "abuse", "abyss", "acorn", "acute", "adapt", "adept", "admit",
@@ -129,155 +129,175 @@ GuessState get_result(string guess, string target) {
 }
 
 struct ConsoleColor {
-	WORD Value;
-	ConsoleColor(WORD value) {
-		Value = value;
-	}
-	ConsoleColor operator | (const ConsoleColor &Other) const {
-		ConsoleColor result = Value;
-		result.Value |= Other.Value;
-		return result;
-	}
-	ConsoleColor operator | (const WORD &Other) const {
-		ConsoleColor result = Value;
-		result.Value |= Other;
-		return result;
-	}
+    WORD Value;
+    ConsoleColor(WORD value) { Value = value; }
+    ConsoleColor operator|(const ConsoleColor &Other) const {
+        ConsoleColor result = Value;
+        result.Value |= Other.Value;
+        return result;
+    }
+    ConsoleColor operator|(const WORD &Other) const {
+        ConsoleColor result = Value;
+        result.Value |= Other;
+        return result;
+    }
 };
 
 namespace colors {
-	const ConsoleColor Black = 0;
-	const ConsoleColor DarkBlue = 1;
-	const ConsoleColor Green = 2;
-	const ConsoleColor LightBlue = 3;
-	const ConsoleColor Red = 4;
-	const ConsoleColor Purple = 5;
-	const ConsoleColor Orange = 6;
-	const ConsoleColor LightGray = 7;
-	const ConsoleColor DarkGray = 8;
-	const ConsoleColor Blue = 9;
-	const ConsoleColor LightGreen = 10;
-	const ConsoleColor Azure = 11;
-	const ConsoleColor Pink = 12;
-	const ConsoleColor LightPurple = 13;
-	const ConsoleColor Yellow = 14;
-	const ConsoleColor DefaultColor = 15;
-}
+const ConsoleColor Black = 0;
+const ConsoleColor DarkBlue = 1;
+const ConsoleColor Green = 2;
+const ConsoleColor LightBlue = 3;
+const ConsoleColor Red = 4;
+const ConsoleColor Purple = 5;
+const ConsoleColor Orange = 6;
+const ConsoleColor DefaultColor = 7;
+const ConsoleColor DarkGray = 8;
+const ConsoleColor Blue = 9;
+const ConsoleColor LightGreen = 10;
+const ConsoleColor Azure = 11;
+const ConsoleColor Pink = 12;
+const ConsoleColor LightPurple = 13;
+const ConsoleColor Yellow = 14;
+const ConsoleColor LightGray = 15;
+} // namespace colors
 
 // 设置颜色
-// 用法1：set_color(colors::Azure); 这个将颜色设置为了浅蓝色，设置后打印的内容可以变色
-// 用法2：cout << set_color(colors::Green) << 内容; 同“用法1”效果但是更简便
-string set_color(const ConsoleColor &color) {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color.Value);
-	return "";
+// 用法1：setColor(colors::Azure);
+// 这个将颜色设置为了浅蓝色，设置后打印的内容可以变色 用法2：cout <<
+// setColor(colors::Green) << 内容; 同“用法1”效果但是更简便
+string setColor(const ConsoleColor &color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color.Value);
+    return "";
 }
 
 // 移动光标
 // 用法1: cur(X坐标, Y坐标); 将鼠标移动到指定位置
-// 用法2: cout << cur(X坐标, Y坐标) << 内容; 同上但是更简便。详情请看 Launch() 函数
-// 注意：X 指横坐标，Y 指纵坐标
+// 用法2: cout << cur(X坐标, Y坐标) << 内容; 同上但是更简便。详情请看 Launch()
+// 函数 注意：X 指横坐标，Y 指纵坐标
 string cur(const short &x, const short &y) {
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { x, y });
-	return "";
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {x, y});
+    return "";
 }
 
 // 设置窗口标题
-// 用法：set_title("我是标题");
-void set_title(const LPCSTR &Name) {
-	SetConsoleTitleA(Name);
-}
-void set_title(const string &Name) {
-	set_title(Name.c_str());
-}
+// 用法：setTitle("我是标题");
+void setTitle(const LPCSTR &Name) { SetConsoleTitleA(Name); }
+void setTitle(const string &Name) { setTitle(Name.c_str()); }
 
-string clear_console() {
-	COORD topLeft  = { 0, 0 };
-	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_SCREEN_BUFFER_INFO screen;
-	DWORD written;
+string clearConsole() {
+    COORD topLeft = {0, 0};
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
 
-	GetConsoleScreenBufferInfo(console, &screen);
-	FillConsoleOutputCharacterA(
-	    console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-	);
-	FillConsoleOutputAttribute(
-	    console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
-	    screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-	);
-	SetConsoleCursorPosition(console, topLeft);
-	return "";
+    GetConsoleScreenBufferInfo(console, &screen);
+    FillConsoleOutputCharacterA(console, ' ', screen.dwSize.X * screen.dwSize.Y,
+                                topLeft, &written);
+    FillConsoleOutputAttribute(
+        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+        screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
+    SetConsoleCursorPosition(console, topLeft);
+    return "";
 }
 
-string clear_console_line(short top) {
-	COORD topLeft  = { (short)0, top };
-	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_SCREEN_BUFFER_INFO screen;
-	DWORD written;
+string clearConsoleLine(short top) {
+    COORD topLeft = {(short)0, top};
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
 
-	GetConsoleScreenBufferInfo(console, &screen);
-	FillConsoleOutputCharacterA(
-	    console, ' ', screen.dwSize.X * (screen.dwSize.Y - top), topLeft, &written
-	);
-	FillConsoleOutputAttribute(
-	    console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
-	    screen.dwSize.X * (screen.dwSize.Y - top), topLeft, &written
-	);
-	SetConsoleCursorPosition(console, topLeft);
-	return "";
+    GetConsoleScreenBufferInfo(console, &screen);
+    FillConsoleOutputCharacterA(console, ' ',
+                                screen.dwSize.X * (screen.dwSize.Y - top),
+                                topLeft, &written);
+    FillConsoleOutputAttribute(
+        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+        screen.dwSize.X * (screen.dwSize.Y - top), topLeft, &written);
+    SetConsoleCursorPosition(console, topLeft);
+    return "";
 }
 
-bool is_pressed(vector<char> args) {
-	for (char ch : args) {
-		if (!(GetKeyState(ch) & 0x8000)) {
-			return 0;
-		}
-	}
-	return 1;
+COORD getCursorPosition() {
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    GetConsoleScreenBufferInfo(console, &screen);
+    return screen.dwCursorPosition;
 }
 
-bool is_pressed(char arg) {
-    return is_pressed((vector<char>){arg});
+bool isPressed(vector<char> args) {
+    for (char ch : args) {
+        if (!(GetKeyState(ch) & 0x8000)) {
+            return 0;
+        }
+    }
+    return 1;
 }
+
+bool isPressed(char arg) { return isPressed((vector<char>){arg}); }
 
 int main() {
     // cout << lower_bound(words.begin(), words.end(), "irate") - words.begin();
     srand(time(0));
-    set_color(colors::DefaultColor);
+    setColor(colors::DefaultColor);
     while (true) {
-        clear_console();
-        cout << set_color(colors::Blue) << "Welcome to Wordle!" << endl << set_color(colors::DefaultColor) << "Version " << Version << endl << endl;
-        cout << set_color(colors::Green) << "* New Game [N]" << endl;
-        cout << set_color(colors::DarkGray) << "* Word List [L]" << endl;
-        set_color(colors::DefaultColor);
+        clearConsole();
+        cout << setColor(colors::Blue) << "Welcome to Wordle!" << endl
+             << setColor(colors::DefaultColor) << "Version " << Version << endl
+             << endl;
+        cout << setColor(colors::Green) << "* New Game [N]" << endl;
+        cout << setColor(colors::LightBlue) << "* Word List [L]" << endl;
+        cout << setColor(colors::DarkGray) << "* Exit [E]" << endl;
+        setColor(colors::DefaultColor);
         while (true) {
             char ch = getch();
             if (ch == 'N' || ch == 'n') {
-                clear_console();
+                clearConsole();
                 string target = words[rand() % words.size()];
                 for (char &ch : target) {
                     ch = ch - 'a' + 'A';
                 }
-                cout << set_color(colors::Blue) << "Wordle" << endl << set_color(colors::DefaultColor) << "Version " << Version << endl << endl;
-                short cursor_top = 3;
+                cout << setColor(colors::Blue) << "Wordle" << endl
+                     << setColor(colors::DefaultColor) << "Version " << Version
+                     << endl
+                     << endl;
                 while (true) {
                     string guess;
-                    cout << set_color(colors::DarkGray) << "Enter something: " << set_color(colors::Yellow);
+                    cout << setColor(colors::DarkGray)
+                         << "Enter something: " << setColor(colors::Yellow);
                     cin >> guess;
                     for (char &ch : guess) {
                         if (ch >= 'a' && ch <= 'z') {
                             ch = ch - 'a' + 'A';
                         }
                     }
-                    cursor_top++;
+                    clearConsoleLine(getCursorPosition().Y - 1);
                     if (guess.size() != Word_Length) {
-                        clear_console_line(--cursor_top);
-                        cout << set_color(colors::Red) << "Your word's length is not " << Word_Length << ". Press any key to continue..." << endl;
+                        cout << setColor(colors::Red)
+                             << "Your word's length is not " << Word_Length
+                             << ". Press any key to continue..." << endl;
                         Sleep(500);
                         getch();
-                        clear_console_line(cursor_top);
+                        clearConsoleLine(getCursorPosition().Y - 1);
                     } else {
-                        clear_console_line(--cursor_top);
-                        cout << set_color(colors::DefaultColor) << guess;
+                        cout << setColor(colors::DefaultColor) << guess;
+                        if (guess == target) {
+                            Sleep(500);
+                            DWORD written;
+                            FillConsoleOutputAttribute(
+                                GetStdHandle(STD_OUTPUT_HANDLE),
+                                colors::Green.Value * 0x10, Word_Length,
+                                {(short)0, getCursorPosition().Y}, &written);
+                            Sleep(500);
+                            cout << endl
+                                 << setColor(colors::Green) << "Success!!!"
+                                 << endl
+                                 << endl
+                                 << setColor(colors::DefaultColor)
+                                 << "Press any key to continue...";
+                            getch();
+                            break;
+                        }
                         GuessState state = get_result(guess, target);
                         for (int i = 0; i < Word_Length; i++) {
                             Sleep(500);
@@ -289,33 +309,36 @@ int main() {
                             }
                             DWORD written;
                             FillConsoleOutputAttribute(
-                                GetStdHandle(STD_OUTPUT_HANDLE), color.Value * 0x10, 1, { (short)i, cursor_top }, &written
-                            );
+                                GetStdHandle(STD_OUTPUT_HANDLE),
+                                color.Value * 0x10, 1,
+                                {(short)i, getCursorPosition().Y}, &written);
                         }
                         cout << endl;
-                        if (state.is_success()) {
-                            cout << set_color(colors::Green) << "Success!!!" << endl << endl << set_color(colors::DefaultColor) << "Press any key to continue...";
-                            getch();
-                            break;
-                        }
-                        cursor_top++;
                     }
                 }
                 break;
             } else if (ch == 'L' || ch == 'l') {
-                clear_console();
-                cout << set_color(colors::Blue) << "Word List" << endl << set_color(colors::Orange);
+                clearConsole();
+                cout << setColor(colors::Blue) << "Word List" << endl
+                     << setColor(colors::Orange);
                 CONSOLE_SCREEN_BUFFER_INFO screen;
-                GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &screen);
+                GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),
+                                           &screen);
                 short sizex = (screen.dwSize.X - 1) / (Word_Length + 1);
                 if (sizex == 0) {
-                    cout << set_color(colors::Red) << "Sorry, the console is too small to display that." << endl << endl << set_color(colors::DefaultColor) << "Press any key to continue...";
+                    cout << setColor(colors::Red)
+                         << "Sorry, the console is too small to display that."
+                         << endl
+                         << endl
+                         << setColor(colors::DefaultColor)
+                         << "Press any key to continue...";
                     getch();
                     break;
                 }
                 for (size_t i = 0; i < words.size(); i++) {
                     CONSOLE_SCREEN_BUFFER_INFO screen;
-                    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &screen);
+                    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),
+                                               &screen);
                     sizex = (screen.dwSize.X - 1) / (Word_Length + 1);
                     if (i % sizex == 0) {
                         cout << endl;
@@ -323,9 +346,22 @@ int main() {
                     cout << words[i] << " ";
                     Sleep(5);
                 }
-                cout << endl << endl << set_color(colors::DefaultColor) << "Press any key to continue...";
+                cout << endl
+                     << endl
+                     << setColor(colors::DefaultColor)
+                     << "Press any key to continue...";
                 getch();
                 break;
+            } else if (ch == 'E' || ch == 'e') {
+                clearConsole();
+                cout << setColor(colors::DefaultColor) << "W:\\ordle\\> "
+                     << setColor(colors::Yellow) << "thanks "
+                     << setColor(colors::DarkGray) << "-for "
+                     << setColor(colors::DefaultColor) << "your "
+                     << setColor(colors::Azure) << "/playing"
+                     << setColor(colors::DefaultColor) << endl
+                     << endl;
+                return 0;
             }
         }
     }
